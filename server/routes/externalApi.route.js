0d61ec09d9a7222efad253a7c1fd4ae75c8b3ecf
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2025-04-04 02:39:21 +0530

    Added: Fetch Usage by email

---
+++ server/routes/externalApi.route.js
@@ -0,0 +1,12 @@
+import express from "express";
+import {
+  getSessionStatus,
+  getClientUsage,
+} from "../controllers/externalApi.controller.js";
+
+const router = express.Router();
+
+router.get("/session-status", getSessionStatus);
+router.get("/getUsage/:email", getClientUsage);
+
+export default router;
