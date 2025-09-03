7fc5a1247040a74b5f6dff4a1c0812e5f8f1b4e7
Ishan Ardithya Bandarigoda <55695411+IshanArdithya@users.noreply.github.com>
2025-09-03 19:05:21 +0530

    restored

---
+++ server/server.js
@@ -0,0 +1,37 @@
+import express from "express";
+import dotenv from "dotenv";
+import { rateLimit } from "express-rate-limit";
+import { MemoryStore } from "express-rate-limit";
+import { connectDB } from "./config/db.js";
+import adminRoutes from "./routes/admin.route.js";
+import externalApiRoutes from "./routes/externalApi.route.js";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+const dataRateLimiter = rateLimit({
+  windowMs: 60 * 1000,
+  max: 50,
+  handler: (req, res) => {
+    res.status(429).json({
+      error: "rate_limit",
+      message: "Too many requests - please try again in a minute",
+      retryAfter: 60,
+    });
+  },
+  store: new MemoryStore(),
+  skip: (req) => req.method !== "GET",
+  keyGenerator: (req) => req.ip,
+});
+
+app.use(express.json());
+connectDB();
+
+app.use("/api/admins", dataRateLimiter, adminRoutes);
+app.use("/api/external", dataRateLimiter, externalApiRoutes);
+
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
